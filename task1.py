fc1e3591be9acc4b11c32594b8e95b3fee5449d3
Mopuri Sushma <mopurisushmareddy@gmail.com>
2026-08-22 09:10:36 +0530

    Remove unnecessary comment from task1.py

--- task1.py
+++ task1.py
@@ -1,7 +1,6 @@
 # Task 1 - Python Programming Internship
 # Maincrafts Technology
 # 1. Sum of Two Numbers
-# -------------------------------
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 print("Sum =", a + b)
