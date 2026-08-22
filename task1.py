228590cc1963d8672beaad24d689cb17c8fbe6f1
Mopuri Sushma <mopurisushmareddy@gmail.com>
2026-08-22 09:05:29 +0530

    Refactor task1.py for better readability

--- task1.py
+++ task1.py
@@ -6,7 +6,6 @@ a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 print("Sum =", a + b)
 print("\n-----------------------")
-
 # 2. Odd or Even
 num = int(input("Enter a number: "))
 if num % 2 == 0:
@@ -14,7 +13,6 @@ if num % 2 == 0:
 else:
     print(num, "is Odd")
 print("\n-----------------------")
-
 # 3. Factorial
 num = int(input("Enter a number: "))
 fact = 1
@@ -22,7 +20,6 @@ for i in range(1, num + 1):
     fact *= i
 print("Factorial =", fact)
 print("\n-----------------------")
-
 # 4. Fibonacci Series
 n = int(input("How many terms? "))
 a, b = 0, 1
@@ -31,12 +28,10 @@ for i in range(n):
     print(a, end=" ")
     a, b = b, a + b
 print("\n\n-----------------------")
-
 # 5. Reverse String
 text = input("Enter a string: ")
 print("Reversed String:", text[::-1])
 print("\n-----------------------")
-
 # 6. Palindrome
 word = input("Enter a word: ")
 if word == word[::-1]:
@@ -44,7 +39,6 @@ if word == word[::-1]:
 else:
     print("Not a Palindrome")
 print("\n-----------------------")
-
 # 7. Leap Year
 year = int(input("Enter Year: "))
 
@@ -53,7 +47,6 @@ if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
 else:
     print(year, "is Not Leap Year")
 print("\n-----------------------")
-
 # 8. Armstrong Number
 num = int(input("Enter a number: "))
 order = len(str(num))
